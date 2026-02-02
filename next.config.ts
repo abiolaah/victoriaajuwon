50c2d866f3c4dd1234bef9c426bec0561fed01a0
abiolaah <oluwapelumi.ajuwon@gmail.com>
2026-02-02 14:51:49 -0700

    configuration file added

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
 import type { NextConfig } from "next";
 
+// const rootDomain =
+//   process.env.NODE_ENV === "production" ? "victoriaajuwon.com" : "localhost";
+
 const nextConfig: NextConfig = {
   /* config options here */
+  typedRoutes: true,
+  rewrites: async () => ({}),
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+    ],
+    qualities: [75, 85, 90, 95],
+  },
 };
 
 export default nextConfig;
